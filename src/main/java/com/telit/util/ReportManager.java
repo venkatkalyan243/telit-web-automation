9cf2fe4581ff22572257035c35968734b6294148
venkatkalyan243 <venkatkalyan243@gmail.com>
2026-06-30 19:47:39 +0530

    ci: introduce GitHub Actions workflow with nightly schedule and manual triggers

--- src/main/java/com/telit/util/ReportManager.java
+++ src/main/java/com/telit/util/ReportManager.java
@@ -3,8 +3,6 @@ package com.telit.util;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
-import java.io.File;
-
 public final class ReportManager {
   private static ExtentReports extent;
 
